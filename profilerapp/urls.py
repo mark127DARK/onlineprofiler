cc7ddb60083f60602c29755add48cae521d14700
mark127DARK <84608380+mark127DARK@users.noreply.github.com>
2022-06-15 16:38:54 +0800

    Added patials

--- profilerapp/urls.py
+++ profilerapp/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from users import views
 
 urlpatterns = [
+    path('', views.home_view, name=''),
     path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
--- users/views.py
+++ users/views.py
@@ -3,5 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# def index(request):
+#     return HttpResponse('hello, World!!!')
+
 def index(request):
-    return HttpResponse('hello, World!!!')
+    return render(request, 'users/index.html')
+
+def home_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request, 'users/index.html')
